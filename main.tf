7ab6911146f3f692c7456bb57e8dc383927e708c
munthalalikhitha-lang <munthalalikhitha@gmail.com>
2026-07-30 18:30:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "one" {
-  ami           = "ami-0884624fc54d115f3"
+  ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "t3.micro"
   tags = {
     Name = "dev-server"

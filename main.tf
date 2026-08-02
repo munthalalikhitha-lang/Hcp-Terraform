84f6a294bf2a26ef2beadf4337ee713e6542bb59
munthalalikhitha-lang <munthalalikhitha@gmail.com>
2026-08-02 11:32:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "ap-south-2"
 }
 resource "aws_instance" "one" {
-count         = 3
+count         = 2
   ami           = "ami-0c8dc555a7e1ca7a3"
   instance_type = "t3.micro"
   tags = {
